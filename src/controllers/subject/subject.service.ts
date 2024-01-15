d3719df584bb3edc047dde8727d95cfa469cf850
carlosaraqueZA <caraque@zonamerica.com>
2024-01-14 21:10:34 -0300

    adding the management of selective subjects

--- src/controllers/subject/subject.service.ts
+++ src/controllers/subject/subject.service.ts
@@ -1,34 +1,35 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { SubjectParentRepository } from 'src/modules/database/repositories/subjectParentRepository.service';
-import { SubjectRepository } from '../../modules/database/repositories/subjectRepository.service';
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { SubjectParentRepository } from "src/modules/database/repositories/subjectParentRepository.service";
+import { SubjectRepository } from "../../modules/database/repositories/subjectRepository.service";
 @Injectable()
 export class SubjectService {
+  constructor(
+    private readonly subjectRepository: SubjectRepository,
+    private readonly subjectParentRepository: SubjectParentRepository
+  ) {}
 
-    constructor(
-        private readonly subjectRepository: SubjectRepository,
-        private readonly subjectParentRepository: SubjectParentRepository
-    ) {}
+  async create(request: {
+    data: any[];
+  }): Promise<{ statusCode: number; message: any }> {
+    const created: any[] = [];
 
-    async create(request: { data: any[] }): Promise<{ statusCode: number, message: any }> {
-      const created: any[] = [];
-    
-      if (!Array.isArray(request.data)) {
-        throw new Error('Invalid data format. "data" should be an array.');
-      }
-    
-      for (const dataItem of request.data) {
-        const subjectParents: number[] = [];
-        const body = {
-          name: dataItem.name,
-          subject_category_id: dataItem.subject_category_id,
-          info: dataItem.info,
-          url: dataItem.url,
-          prefix: dataItem.prefix
-        };
-    
+    if (!Array.isArray(request.data)) {
+      throw new Error('Invalid data format. "data" should be an array.');
+    }
 
-    
-/*         for (const parent of dataItem?.subjectParent) {
+    for (const dataItem of request.data) {
+      const subjectParents: number[] = [];
+      const body = {
+        name: dataItem.name,
+        subject_category_id: dataItem.subject_category_id,
+        info: dataItem.info,
+        url: dataItem.url,
+        selective: dataItem.selective,
+        selectiveSubjects: dataItem.selectiveSubjects,
+        prefix: dataItem.prefix,
+      };
+
+      /*         for (const parent of dataItem?.subjectParent) {
           if (typeof parent.key !== 'undefined' || parent.id) {
             if (typeof parent.key !== 'undefined') {
               subjectParents.push(created[parent.key].id);
@@ -38,85 +39,82 @@ export class SubjectService {
             }
           }
         } */
-    
-        try {
-          const subject = await this.subjectRepository.create(body);
-          created.push(subject);
-    
-          // Guardar los padres
-/*           for (const subjectParent of subjectParents) {
+
+      try {
+        const subject = await this.subjectRepository.create(body);
+        created.push(subject);
+
+        // Guardar los padres
+        /*           for (const subjectParent of subjectParents) {
             await this.subjectParentRepository.create({
               subject_id: subject.id,
               subject_parent_id: subjectParent
             });
           } */
-        } catch (error) {
-         console.error("ERROR: " ,error)
-        }
+      } catch (error) {
+        console.error("ERROR: ", error);
       }
-    
-      return { statusCode: HttpStatus.OK, message: created };
     }
-    
 
-   async getAll(career){
-      const subjects = await this.subjectRepository.getAll(career)
-      return subjects;
-   }
+    return { statusCode: HttpStatus.OK, message: created };
+  }
 
-   async getById(id:number){
-      const subject = await this.subjectRepository.getById(id)
-      return subject;
-   }
+  async getAll(career) {
+    const subjects = await this.subjectRepository.getAll(career);
+    return subjects;
+  }
 
-   async update(request: any){
-      const created: any[] = [];
-      for (let i = 0; i < request.data.length; i++) {
-         let subject
-         let subjectParents = []
-         let body = {
-            name: request.data[i].name,
-            subject_category_id: request.data[i].subject_category_id,
-            info: request.data[i].info,
-            url: request.data[i].url,
-            prefix: request.data[i].prefix
-         }
-         // for (let parent of request.data[i].subjectParent) {
-         //    if (parent.key || parent.key == 0 || parent.id) {
-         //       if (parent.key || parent.key == 0) {
-         //          subjectParents.push(created[`${parent.key}`].id)
-         //       }
-         //       if (parent.id) {
-         //          subjectParents.push(parent.id)
-         //       }
-         //    }
-         // }
-         if (request.data[i].id) {
-            subject = await this.subjectRepository.update(request.data[i].id, body)
-         }else{
-            subject = await this.subjectRepository.create(body) 
-         }
-         created.push(subject);
-         // save parents
-         // await this.subjectParentRepository.deleteMany(request.data[i].deleteParent, subject.id)
-         // for (let subjectParent of subjectParents) {
-         //    await this.subjectParentRepository.create({
-         //       subject_id: subject.id,
-         //       subject_parent_id: subjectParent
-         //    })
-         // }
+  async getById(id: number) {
+    const subject = await this.subjectRepository.getById(id);
+    return subject;
+  }
+
+  async update(request: any) {
+    const created: any[] = [];
+    for (let i = 0; i < request.data.length; i++) {
+      let subject;
+      let subjectParents = [];
+      let body = {
+        name: request.data[i].name,
+        subject_category_id: request.data[i].subject_category_id,
+        info: request.data[i].info,
+        url: request.data[i].url,
+        selective: request.data[i].selective,
+        selectiveSubjects: request.data[i].selectiveSubjects,
+        prefix: request.data[i].prefix,
+      };
+      // for (let parent of request.data[i].subjectParent) {
+      //    if (parent.key || parent.key == 0 || parent.id) {
+      //       if (parent.key || parent.key == 0) {
+      //          subjectParents.push(created[`${parent.key}`].id)
+      //       }
+      //       if (parent.id) {
+      //          subjectParents.push(parent.id)
+      //       }
+      //    }
+      // }
+      if (request.data[i].id) {
+        subject = await this.subjectRepository.update(request.data[i].id, body);
+      } else {
+        subject = await this.subjectRepository.create(body);
       }
-      // await this.subjectRepository.deleteMany(request.deleteData)
-      return created
-   }
+      created.push(subject);
+      // save parents
+      // await this.subjectParentRepository.deleteMany(request.data[i].deleteParent, subject.id)
+      // for (let subjectParent of subjectParents) {
+      //    await this.subjectParentRepository.create({
+      //       subject_id: subject.id,
+      //       subject_parent_id: subjectParent
+      //    })
+      // }
+    }
+    // await this.subjectRepository.deleteMany(request.deleteData)
+    return created;
+  }
 
-   async delete(id: number){
-      const subject = await this.subjectRepository.delete(id)
+  async delete(id: number) {
+    const subject = await this.subjectRepository.delete(id);
 
-
-      return {statusCode: 200, message: 'removed'}
-   }
-
-     
-
+    return { statusCode: 200, message: "removed" };
+  }
 }
