5b3dd4ded73838af43be7ea0df9322b8d9d7f70b
carlosaraque22 <cararqueu@gmail.com>
2024-01-17 03:32:34 -0300

    fix error in suggestion module

--- src/controllers/subjectCategory/subjectCategory.module.ts
+++ src/controllers/subjectCategory/subjectCategory.module.ts
@@ -1,13 +1,12 @@
-import { Module } from '@nestjs/common';
-import { SubjectCategoryService } from './subjectCategory.service';
-import { PassportModule } from '@nestjs/passport';
-import {SubjectCategoryController} from "./subjectCategory.controller";
-import { SharedModule } from 'src/modules/shared/shared.module';
+import { Module } from "@nestjs/common";
+import { SubjectCategoryService } from "./subjectCategory.service";
+import { PassportModule } from "@nestjs/passport";
+import { SubjectCategoryController } from "./subjectCategory.controller";
+import { SharedModule } from "src/modules/shared/shared.module";
 
 @Module({
-    controllers: [SubjectCategoryController],
-    imports: [PassportModule,
-         SharedModule],
-    providers: [SubjectCategoryService],
+  controllers: [SubjectCategoryController],
+  imports: [PassportModule, SharedModule],
+  providers: [SubjectCategoryService],
 })
 export class SubjectCategoryModule {}
--- src/controllers/suggestion/suggestion.module.ts
+++ src/controllers/suggestion/suggestion.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { SuggestionController } from "./suggestion.controller";
+import { PassportModule } from "@nestjs/passport";
 import { SuggestionService } from "./suggestion.service";
+import { SharedModule } from "src/modules/shared/shared.module";
 
 @Module({
   controllers: [SuggestionController],
+  imports: [PassportModule, SharedModule],
   providers: [SuggestionService],
 })
-export class AppModule {}
+export class SuggestionModule {}
