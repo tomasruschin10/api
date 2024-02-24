671efd7f2e3579118a97eb34dc66dfa5aa92039d
Carlosaraque22 <cararqueu@gmail.com>
2024-02-24 04:25:13 -0300

    adding values for new note flow

--- src/models/userSubject.entity.ts
+++ src/models/userSubject.entity.ts
@@ -5,48 +5,63 @@ import { User } from './user.entity';
 import { Subject } from './subject.entity';
 import { ExtraScore } from './extraScore.entity';
 
-@Entity({name:'user_subjects'})
+@Entity({ name: 'user_subjects' })
 export class UserSubject {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column()
-  user_id: number;
+    @Column()
+    user_id: number;
 
-  @Column()
-  subject_id: number;
+    @Column()
+    subject_id: number;
 
-  @Column({nullable: true})
-  score: number;
+    @Column({ nullable: true })
+    score: number;
 
-  @Column({default: 0, type: 'boolean'})
-  finish: number;
+    @Column({ nullable: true })
+    chair: string;
 
-  @CreateDateColumn({type: "timestamp"})
-  created_at: Moment
+    @Column({ nullable: true })
+    requirement: number;
 
-  @UpdateDateColumn({type: "timestamp", nullable: true})
-  updated_at: Moment
+    @Column({ nullable: true })
+    qualityOfTeachers: number;
 
-  //relations
-  @ManyToOne(() => User, user => user.id, {onDelete: 'CASCADE', onUpdate: 'CASCADE'})
-  @JoinColumn({
-      name: 'user_id',
-      referencedColumnName: 'id'
-  })
-  user: User;
+    @Column({ nullable: true })
+    practicalJobs: number;
 
-  @ManyToOne(() => Subject, subject => subject.id, {onDelete: 'CASCADE', onUpdate: 'CASCADE'})
-  @JoinColumn({
-      name: 'subject_id',
-      referencedColumnName: 'id'
-  })
-  subject: Subject;
+    @Column({ nullable: true })
+    cost: number;
 
-  @OneToMany(() => ExtraScore, extra_score => extra_score.user_subject, {onDelete: 'CASCADE', onUpdate: 'CASCADE'})
-  @JoinColumn({
-      name: 'id',
-      referencedColumnName: 'extra_score_id'
-  })
-  extra_score: ExtraScore[];
+    @Column({ default: 0, type: 'boolean' })
+    finish: number;
+
+    @CreateDateColumn({ type: "timestamp" })
+    created_at: Moment
+
+    @UpdateDateColumn({ type: "timestamp", nullable: true })
+    updated_at: Moment
+
+    //relations
+    @ManyToOne(() => User, user => user.id, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @JoinColumn({
+        name: 'user_id',
+        referencedColumnName: 'id'
+    })
+    user: User;
+
+    @ManyToOne(() => Subject, subject => subject.id, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @JoinColumn({
+        name: 'subject_id',
+        referencedColumnName: 'id'
+    })
+    subject: Subject;
+
+    @OneToMany(() => ExtraScore, extra_score => extra_score.user_subject, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @JoinColumn({
+        name: 'id',
+        referencedColumnName: 'extra_score_id'
+    })
+    extra_score: ExtraScore[];
 }
