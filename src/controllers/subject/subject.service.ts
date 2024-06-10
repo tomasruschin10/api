5bcd8a748c52e1c540c27c598afce79af634baef
Carlosaraque22 <cararqueu@gmail.com>
2024-06-10 17:42:26 -0300

    fix

--- src/controllers/subject/subject.service.ts
+++ src/controllers/subject/subject.service.ts
@@ -77,53 +77,50 @@ export class SubjectService {
     const created: any[] = [];
 
     for (let i = 0; i < request.data.length; i++) {
-      let subject;
-      let subjectParents = []
-      let body = {
-        name: request.data[i].name,
-        subject_category_id: request.data[i].subject_category_id,
-        info: request.data[i].info,
-        url: request.data[i].url,
-        selective: request.data[i].selective,
-        selectiveSubjects: request.data[i].selectiveSubjects,
-        chairs: request.data[i].chairs,
-        prefix: request.data[i].prefix,
-      };
+        let subject;
+        let subjectParents = []
+        let body = {
+            name: request.data[i].name,
+            subject_category_id: request.data[i].subject_category_id,
+            info: request.data[i].info,
+            url: request.data[i].url,
+            selective: request.data[i].selective,
+            selectiveSubjects: request.data[i].selectiveSubjects,
+            chairs: request.data[i].chairs,
+            prefix: request.data[i].prefix,
+        };
 
-
-      if (request.data[i].id) {
-        subject = await this.subjectRepository.update(request.data[i].id, body);
-      } else {
-        subject = await this.subjectRepository.create(body);
-      }
-
-
-      await this.subjectParentRepository.deleteAllBySubjectId(subject.id);
-
-      for (const parent of request.data[i].subjectParent) {
-        let parentId;
-        if (parent.key || parent.key === 0 || parent.id) {
-          if (parent.key || parent.key === 0) {
-            parentId = created[parent.key].id;
-          }
-          if (parent.id) {
-            parentId = parent.id;
-          }
-          subjectParents.push(parentId);
+       
+        if (request.data[i].id) {
+            subject = await this.subjectRepository.update(request.data[i].id, body);
+        } else {
+            subject = await this.subjectRepository.create(body);
         }
-      }
+      
 
+        await this.subjectParentRepository.deleteAllBySubjectId(subject.id);
 
-      for (const parentId of subjectParents) {
-        await this.subjectParentRepository.create({ subject_id: subject.id, subject_parent_id: parentId });
-      }
+        for (let parent of request.data[i].subjectParent) {
+            if (parent.key || parent.key === 0 || parent.id) {
+                if (parent.key || parent.key === 0) {
+                    subjectParents.push(created[parent.key].id);
+                }
+                if (parent.id) {
+                    subjectParents.push(parent.id);
+                }
+            }
+        }
 
-      subject.subjectParents = subjectParents;
-
-      created.push(subject);
+        for (let subjectParent of subjectParents) {
+            await this.subjectParentRepository.create({
+                subject_id: subject.id,
+                subject_parent_id: subjectParent
+            });
+        }
+        created.push(subject);
     }
     return { created };
-  }
+}
 
   async delete(id: number) {
     const subject = await this.subjectRepository.delete(id);
