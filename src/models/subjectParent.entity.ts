461c430d8c58031112d4060dcdd8487a21907f23
Carlosaraque22 <cararqueu@gmail.com>
2024-07-19 14:31:57 -0300

    fix

--- src/models/subjectParent.entity.ts
+++ src/models/subjectParent.entity.ts
@@ -3,24 +3,10 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany, OneToOne, ManyToOne,
 import { Moment } from 'moment';
 import { Subject } from './subject.entity';
 
-@Entity()
 export class OrSubjectParents {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
   subject_parent_id: number;
-
-  @Column()
   or_subject_id: number;
-
-  @ManyToOne(() => Subject, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
-  @JoinColumn({ name: 'or_subject_id', referencedColumnName: 'id' })
-  orSubject: Subject;
-
-  @ManyToOne(() => SubjectParent, subjectParent => subjectParent.orSubjectParents, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
-  @JoinColumn({ name: 'subject_parent_id', referencedColumnName: 'id' })
-  subjectParent: SubjectParent;
+  parent: Subject;
 }
 
 @Entity({ name: 'subject_parents' })
@@ -57,6 +43,6 @@ export class SubjectParent {
   })
   parent: Subject;
 
-  @OneToMany(() => OrSubjectParents, orSubjectParents => orSubjectParents.subjectParent, { cascade: true })
+  @Column('jsonb', { nullable: true })
   orSubjectParents: OrSubjectParents[];
 }
