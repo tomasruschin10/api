1a6f03511582d1938ac64ff8436ae588682296c1
Carlosaraque22 <cararqueu@gmail.com>
2024-07-19 14:38:45 -0300

    fix

--- src/models/subjectParent.entity.ts
+++ src/models/subjectParent.entity.ts
@@ -3,13 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany, OneToOne, ManyToOne,
 import { Moment } from 'moment';
 import { Subject } from './subject.entity';
 
-export class OrSubjectParents {
-  subject_parent_id: number;
-  or_subject_id: number;
-  parent: Subject;
-}
-
-@Entity({ name: 'subject_parents' })
+@Entity({name:'subject_parents'})
 export class SubjectParent {
   @PrimaryGeneratedColumn()
   id: number;
@@ -20,29 +14,27 @@ export class SubjectParent {
   @Column()
   subject_parent_id: number;
 
-  @CreateDateColumn({ type: "timestamp" })
+  @CreateDateColumn({type: "timestamp"})
   created_at: Moment
 
-  @UpdateDateColumn({ type: "timestamp", nullable: true })
+  @UpdateDateColumn({type: "timestamp", nullable: true})
   updated_at: Moment
 
 
   //relations
 
-  @ManyToOne(() => Subject, subject => subject.id, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  @ManyToOne(() => Subject, subject => subject.id , {onDelete: 'CASCADE', onUpdate: 'CASCADE'})
   @JoinColumn({
-    name: 'subject_id',
-    referencedColumnName: 'id'
+      name: 'subject_id',
+      referencedColumnName: 'id'
   })
   subject: Subject;
 
-  @ManyToOne(() => Subject, parent => parent.id, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  @ManyToOne(() => Subject, parent => parent.id , {onDelete: 'CASCADE', onUpdate: 'CASCADE'})
   @JoinColumn({
-    name: 'subject_parent_id',
-    referencedColumnName: 'id'
+      name: 'subject_parent_id',
+      referencedColumnName: 'id'
   })
   parent: Subject;
 
-  @Column('jsonb', { nullable: true })
-  orSubjectParents: OrSubjectParents[];
 }
